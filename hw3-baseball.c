c65cb3bce4cc670ae8b8de190f39f0560b00fb8c
natrucha <natapueyo@gmail.com>
2017-08-08 22:56:27 -0700

    Final clean version

--- hw3-baseball.c
+++ hw3-baseball.c
@@ -1,17 +1,13 @@
 #include<stdio.h>
 #include<ctype.h>
 #include<string.h>
-// works fine on baseball.txt, but doen't on baseball2.txt
-// looks like it overwrites over the previous string
-// but if the previous string is longer, those slots don't get overwritten
 
 int main(int argc, char const *argv[]) {
   // need to fscanf whatever txt file
-  FILE *score_card;
-  int input_status;
+  FILE *score_card; // text file useed to get data
+  int input_status; // number of inputs obtained from the TXT file
   int player_number;
   char record[27]; // record given in hits, outs, walks, sacrifices, and hits by pitch
-  //int counter_bats; // counts the total at-bats
   int counter_hits; // counts number of hits
   int counter_outs; // counts number of outs
   double average; // player's batting average
@@ -22,38 +18,32 @@ int main(int argc, char const *argv[]) {
     // ()%s with a non-determined length is a really bad idea safety-wise)
     input_status = fscanf(score_card, "%d %s", &player_number, &record);
 
-    //printf("player number: %d, record: %s\n", player_number, record);
-
     while (input_status == 2){
-      // add up all the at-bat
-      //counter_bats = 0;
+      // set up counters for hits and outs to get the final average
       counter_hits = 0;
       counter_outs = 0;
       for (int i = 0; i < sizeof(record); i++) {
         char letter = record[i];
-        // checks if the particular array element is filled
+        // checks if the particular array element is filled with a letter
         if (isalpha(letter)) {
-          // if filled, add to the counter for batting
-          //counter_bats++;
-          //printf("%d\n", counter_bats);
-          //printf("letter of record at loop: %c\n", letter);
+          // if filled, determined if it is one of the two important results
           if (letter == 'H') {
             // add up all the hits
             counter_hits++;
-            //printf("Hit added\n");
           } else if (letter == 'O') {
-            // subtract any at-bat that was an out
+            // add up all the outs together
             counter_outs++;
-            //printf("Out added\n");
           }
         }
 
       }
-      // divide the number of hits
+      // divide the number of hits by the number of hits + bats (at-bats)
       average = (double) counter_hits / (counter_hits + counter_outs);
       printf("Player %d's record: %s\n", player_number, record);
       printf("Player %d's batting average: %.3lf\n\n", player_number, average);
+      // clear the string for the next player's records
       memset(record, '0', 27);
+      // obtain next player's information from the TXT file
       input_status = fscanf(score_card, "%d %s", &player_number, &record);
     }
   }
