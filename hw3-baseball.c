11147a0f9f9ea580876f74ed7c3b9fe07c47da61
natrucha <natapueyo@gmail.com>
2017-08-08 20:08:12 -0700

    removed strcmp to compare the char in each record
    looks like it's close but it is missing a way to overwrite previous string completely

--- hw3-baseball.c
+++ hw3-baseball.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
-#include<string.h>
-// wrong thing to use because it has to be a constant string
-// mine's not a constant string...
+#include<ctype.h>
+// works fine on baseball.txt, but doen't on baseball2.txt
+// looks like it overwrites over the previous string
+// but if the previous string is longer, those slots don't get overwritten
+
 
 int main(int argc, char const *argv[]) {
   // need to fscanf whatever txt file
   FILE *score_card;
   int input_status;
   int player_number;
-  char record[30]; // record given in hits, outs, walks, sacrifices, and hits by pitch
+  char record[27]; // record given in hits, outs, walks, sacrifices, and hits by pitch
   int counter_bats; // counts the total at-bats
   int counter_hits; // counts number of hits
+  int counter_outs; // counts number of outs
   double average; // player's batting average
 
   if (argc == 2) {
@@ -19,24 +22,38 @@ int main(int argc, char const *argv[]) {
     // ()%s with a non-determined length is a really bad idea safety-wise)
     input_status = fscanf(score_card, "%d %s", &player_number, &record);
 
+    printf("player number: %d, record: %s\n", player_number, record);
+
     while (input_status == 2){
       // add up all the at-bat
       counter_bats = 0;
       counter_hits = 0;
+      counter_outs = 0;
       for (int i = 0; i < sizeof(record); i++) {
-        counter_bats++;
-        if (strcmp(record[i], "H") == 0) {
-          // add up all the hits
-          counter_hits++;
-        } else if (strcmp(record[i], "O") == 0) {
-          // subtract any at-bat that was an out
-          counter_bats--;
+        char letter = record[i];
+        // checks if the particular array element is filled
+        if (isalpha(letter)) {
+          // if filled, add to the counter for batting
+          counter_bats++;
+          printf("%d\n", counter_bats);
+          printf("letter of record at loop: %c\n", letter);
+          if (letter == 'H') {
+            // add up all the hits
+            counter_hits++;
+            printf("Hit added\n");
+          } else if (letter == 'O') {
+            // subtract any at-bat that was an out
+            counter_outs++;
+            printf("Out added\n");
+          }
         }
+
       }
       // divide the number of hits
-      average = (double) counter_hits / counter_bats;
+      average = (double) counter_hits / (counter_hits + counter_outs);
       printf("Player %d's record: %s\n", player_number, record);
       printf("Player %d's batting average: %.3lf\n\n", player_number, average);
+      input_status = fscanf(score_card, "%d %s", &player_number, &record);
     }
   }
 
