669e71e3ee192da2e6935ec97aa641900b498839
Natalie Pueyo <natrucha@localhost.localdomain>
2017-08-29 16:34:40 -0700

    cleaned and added comments

--- hw4-line.c
+++ hw4-line.c
@@ -27,71 +27,42 @@ void display_slope_intcpt (double m, double y_int);
 
 int main(int argc, char const *argv[]) {
   // this is a pain in the butt....
-  int conv_form;
-  int x_coor, y_coor;
-  int x_coor_1, y_coor_1;
-  double slope;
-  double b;
+  int conv_form; // choice to use either two points or a point and a slope for the calculations
+  int x_coor, y_coor; // first point's coordiantes
+  int x_coor_1, y_coor_1; // second point's coordinates
+  double slope; // slope of the line
+  double b; // y-intercept of the line
 
   // set up the menu by having the user choose one of the two options
   conv_form = get_problem();
-  /*printf("Select the form that you would like to convert to slope-intercept form:\n");
-  printf("1) Two-point form (you know two points on the line)\n");
-  printf("2) Point-slope form (you know the line's slope and one point)\n");
-  printf("=> ");
-  scanf("%d", &conv_form);
-  printf("choice %d\n", conv_form);*/
 
   // depending on the user's choice, the options will change the menu to reflect
   // which variables get filled and passed to the main program
-  if (conv_form == 1) {
+  if (conv_form == 1) { // if two point form
+    // ask the user for the four coordiantes that make up the points
     get2_pt(&x_coor, &y_coor, &x_coor_1, &y_coor_1);
-    /*printf("Enter the x-y coordinates of the first point separted by a space=> ");
-    scanf("%d %d", &x_coor, &y_coor);
-    printf("Enter the x-y coordinates of the second point separated by a space=> ");
-    scanf("%d %d", &x_coor_1, &y_coor_1);
-    */
-  } else if (conv_form == 2) {
+
+  } else if (conv_form == 2) { // if slope and point
+    // ask the user for the slope and the point
     get_pt_slope(&slope, &x_coor, &y_coor);
-    /*printf("Enter the slope=> ");
-    scanf("%lf", &slope);
-    printf("Enter the x-y coordinates of the point separated by a space=> ");
-    scanf("%d %d", &x_coor, &y_coor);
-    */
+
   }
 
+  // depending on the user's choice, different calculations and different display messages come up
   if (conv_form == 1) {
+    // calculate the slope and y-intercept using the two points
     slope_intcpt_from2_pt(x_coor, y_coor, x_coor_1, y_coor_1, &slope, &b);
-    /*slope = (double) (y_coor_1 - y_coor) / (x_coor_1 - x_coor);
-    b = (double) (-x_coor * slope) + y_coor;*/
-
+    // displays the equation used to calculate the slope
     display2_pt(x_coor, y_coor, x_coor_1, y_coor_1);
-    /*
-    printf("\nTwo-point form\n");
-    printf("      (%.2lf- %.2lf)\n", (double) y_coor_1, (double) y_coor);
-    printf("  m = -------------\n");
-    printf("      (%.2lf - %.2lf)\n\n", (double) x_coor_1, (double) x_coor);
-    */
+    // displays the final slope-intercept equation
     display_slope_intcpt (slope, b);
-    /*
-    printf("Slope-intercept form\n");
-    printf("  y = %.2lfx + %.2lf\n", slope, b);
-    */
-
   } else if (conv_form == 2) {
+    // if slope is given, the y-intercept, or b, is calculated using the point and slope 
     b = intcpt_from_pt_slope(x_coor, y_coor, slope);
-    //b = (double) (-x_coor * slope) + y_coor;
-
+    // the equation used to calculate the y-intercept is displayed
     display_pt_slope(x_coor, y_coor, slope);
-    /*
-    printf("\nPoint-slope form\n");
-    printf("  y - %.2lf = %.2lf(x - %.2lf)\n\n", (double) y_coor, slope, (double) x_coor);
-    */
+    // displayes the final slope-intercept equation
     display_slope_intcpt (slope, b);
-    /*
-    printf("Slope-intercept form\n");
-    printf("  y = %.2lfx + %.2lf\n", slope, b);
-    */
 
   }
 
@@ -166,10 +137,13 @@ void display_pt_slope(int x, int y, double m){
 
 void display_slope_intcpt (double m, double y_int){
   if (y_int < 0){
+    // if the y-intercept is negative, the slope-intercept form has to change
+    // because the sign is presented separately. This could be done for other display cases
+    // if so desired (especially for display2_pt)
     y_int = -y_int;
     printf("Slope-intercept form\n");
     printf("  y = %.2lfx - %.2lf\n", m, y_int);
-  } else {
+  } else { // if not negative (including zero) the equation should remain positive
     printf("Slope-intercept form\n");
     printf("  y = %.2lfx + %.2lf\n", m, y_int);
   }
