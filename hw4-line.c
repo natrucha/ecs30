e2d495c6861fbbaf179361b09eb67d64d8d8078a
Natalie Pueyo <natrucha@localhost.localdomain>
2017-08-29 17:08:04 -0700

    created a while loop to allow the user to make another conversion

--- hw4-line.c
+++ hw4-line.c
@@ -3,18 +3,18 @@
 int get_problem();
 
 void get2_pt(int *x1, int *y1, // first point coordinates
-             int *x2, int *y2 // second point coordinates
-           );
+  int *x2, int *y2 // second point coordinates
+);
 
 void get_pt_slope( double *m, // slope
-                  int *x, int *y // point coordinates
-                );
+  int *x, int *y // point coordinates
+);
 
 void slope_intcpt_from2_pt(int x1, int y1, // first point coordinates
-                           int x2, int y2, // second point coordinates
-                           double *m, // slope
-                           double *b // y-intercept
-                         );
+  int x2, int y2, // second point coordinates
+  double *m, // slope
+  double *b // y-intercept
+);
 
 double intcpt_from_pt_slope(int x, int y, double m);
 
@@ -32,38 +32,54 @@ int main(int argc, char const *argv[]) {
   int x_coor_1, y_coor_1; // second point's coordinates
   double slope; // slope of the line
   double b; // y-intercept of the line
+  int repeat = 1; // determines if the program has to run again
+  char another; // choice to do another conversion
 
-  // set up the menu by having the user choose one of the two options
-  conv_form = get_problem();
+  while (repeat == 1) {
 
-  // depending on the user's choice, the options will change the menu to reflect
-  // which variables get filled and passed to the main program
-  if (conv_form == 1) { // if two point form
-    // ask the user for the four coordiantes that make up the points
-    get2_pt(&x_coor, &y_coor, &x_coor_1, &y_coor_1);
+    // set up the menu by having the user choose one of the two options
+    conv_form = get_problem();
 
-  } else if (conv_form == 2) { // if slope and point
-    // ask the user for the slope and the point
-    get_pt_slope(&slope, &x_coor, &y_coor);
+    // make repeat 0 so that unless explicitly asked, the program ends
+    repeat = 0;
 
-  }
+    // depending on the user's choice, the options will change the menu to reflect
+    // which variables get filled and passed to the main program
+    if (conv_form == 1) { // if two point form
+      // ask the user for the four coordiantes that make up the points
+      get2_pt(&x_coor, &y_coor, &x_coor_1, &y_coor_1);
 
-  // depending on the user's choice, different calculations and different display messages come up
-  if (conv_form == 1) {
-    // calculate the slope and y-intercept using the two points
-    slope_intcpt_from2_pt(x_coor, y_coor, x_coor_1, y_coor_1, &slope, &b);
-    // displays the equation used to calculate the slope
-    display2_pt(x_coor, y_coor, x_coor_1, y_coor_1);
-    // displays the final slope-intercept equation
-    display_slope_intcpt (slope, b);
-  } else if (conv_form == 2) {
-    // if slope is given, the y-intercept, or b, is calculated using the point and slope 
-    b = intcpt_from_pt_slope(x_coor, y_coor, slope);
-    // the equation used to calculate the y-intercept is displayed
-    display_pt_slope(x_coor, y_coor, slope);
-    // displayes the final slope-intercept equation
-    display_slope_intcpt (slope, b);
+    } else if (conv_form == 2) { // if slope and point
+      // ask the user for the slope and the point
+      get_pt_slope(&slope, &x_coor, &y_coor);
 
+    }
+
+    // depending on the user's choice, different calculations and different display messages come up
+    if (conv_form == 1) {
+      // calculate the slope and y-intercept using the two points
+      slope_intcpt_from2_pt(x_coor, y_coor, x_coor_1, y_coor_1, &slope, &b);
+      // displays the equation used to calculate the slope
+      display2_pt(x_coor, y_coor, x_coor_1, y_coor_1);
+      // displays the final slope-intercept equation
+      display_slope_intcpt (slope, b);
+    } else if (conv_form == 2) {
+      // if slope is given, the y-intercept, or b, is calculated using the point and slope
+      b = intcpt_from_pt_slope(x_coor, y_coor, slope);
+      // the equation used to calculate the y-intercept is displayed
+      display_pt_slope(x_coor, y_coor, slope);
+      // displayes the final slope-intercept equation
+      display_slope_intcpt (slope, b);
+
+    }
+
+    printf("Do another conversion (Y or N)=> ");
+    scanf(" %c", &another);
+    printf("\n");
+
+    if (another == 'Y' || another == 'y') {
+      repeat = 1;
+    }
   }
 
   return 0;
@@ -84,8 +100,8 @@ int get_problem(){
 }
 
 void get2_pt(int *x1, int *y1, // first point coordinates
-             int *x2, int *y2 // second point coordinates
-            ){
+  int *x2, int *y2 // second point coordinates
+){
   printf("Enter the x-y coordinates of the first point separted by a space=> ");
   scanf("%d %d", x1, y1); // gets the coordinate for the first point
   printf("Enter the x-y coordinates of the second point separated by a space=> ");
@@ -93,8 +109,8 @@ void get2_pt(int *x1, int *y1, // first point coordinates
 }
 
 void get_pt_slope( double *m, // slope
-                   int *x, int *y // point coordinates
-                 ){
+  int *x, int *y // point coordinates
+){
   printf("Enter the slope=> ");
   scanf("%lf", m);
   printf("Enter the x-y coordinates of the point separated by a space=> ");
@@ -102,10 +118,10 @@ void get_pt_slope( double *m, // slope
 }
 
 void slope_intcpt_from2_pt(int x1, int y1, // first point coordinates
-                           int x2, int y2, // second point coordinates
-                           double *m, // slope
-                           double *y_int // y-intercept
-                           ){
+  int x2, int y2, // second point coordinates
+  double *m, // slope
+  double *y_int // y-intercept
+){
   // calculates the slope using the coordinates of the two points
   *m = (double) (y2 - y1) / (x2 - x1);
   // calculates the y-intercept using the calculated slope and the first point
@@ -122,18 +138,18 @@ double intcpt_from_pt_slope(int x, int y, double m){
 }
 
 void display2_pt(int x1, int y1, int x2, int y2){
-   // takes the four coordinates of submitted two points
-   // displayes a two-point line equation with a heading
-   printf("\nTwo-point form\n");
-   printf("      (%.2lf- %.2lf)\n", (double) y2, (double) y1);
-   printf("  m = -------------\n");
-   printf("      (%.2lf - %.2lf)\n\n", (double) x2, (double) x1);
- }
+  // takes the four coordinates of submitted two points
+  // displayes a two-point line equation with a heading
+  printf("\nTwo-point form\n");
+  printf("      (%.2lf- %.2lf)\n", (double) y2, (double) y1);
+  printf("  m = -------------\n");
+  printf("      (%.2lf - %.2lf)\n\n", (double) x2, (double) x1);
+}
 
 void display_pt_slope(int x, int y, double m){
-   printf("\nPoint-slope form\n");
-   printf("  y - %.2lf = %.2lf(x - %.2lf)\n\n", (double) y, m, (double) x);
- }
+  printf("\nPoint-slope form\n");
+  printf("  y - %.2lf = %.2lf(x - %.2lf)\n\n", (double) y, m, (double) x);
+}
 
 void display_slope_intcpt (double m, double y_int){
   if (y_int < 0){
